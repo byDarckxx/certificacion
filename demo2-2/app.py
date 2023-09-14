aaf9c378bfe4e30958c85cab4899f30fd5fc3387
byDarckxx <robe.rivera@duocuc.cl>
2023-09-14 19:34:41 -0300

    Se modifica linea que impide get

--- demo2-2/app.py
+++ demo2-2/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 def hello():
     html = "<h3>Hello {name}!</h3> <b>Hostname:</b> {hostname} <br/>"
-    return html.format(name=os.gatenv("NAME", "world"), hostname=socket.gethostname())
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4000)
